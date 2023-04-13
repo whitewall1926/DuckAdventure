07503f5962a9eebbb25f4fa3ba9f1e95c57b1e5d
whitewall1926 <whitewall_9@163.com>
2023-04-13 23:04:15 +0800

    second commit: jump

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public PlayerController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            controller.Play();
+        }
+    }
+}
